b33493486d72f67f52ed908eef28e1c28658815a
Perttu <perttu@gmail.com>
2015-10-14 15:37:31 +0300

    Add some content

--- app/controller.js
+++ app/controller.js
@@ -1,3 +1,13 @@
 angular.module('myApp').controller('AppCtrl', function($scope) {
   $scope.message = 'Hello from controller.js';
+
+  $scope.companies = [
+    { name: "TietoEnator", beginTime: "06/2001", endTime: "03/2004"},
+    { name: "Sininen Meteoriitti", beginTime: "03/2004", endTime: ""},
+    { name: "TJ Group", beginTime: "02/2000", endTime: "06/2001"}
+  ];
+
+  $scope.hideJobHistory = false;
+  $scope.hideAboutMe = false;
+
 });
